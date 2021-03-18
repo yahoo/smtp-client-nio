797ce1a994d8a3f7d087358923c8e904df4d06a3
Fan Su <fansu.dev@gmail.com>
2021-03-18 11:38:23 -0700

    Update gpg encryption files

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,8 +4,8 @@ if [ "${TRAVIS_BRANCH}" == 'master' ] && [ "${TRAVIS_PULL_REQUEST}" == 'false' ]
 
     openssl aes-256-cbc -pass pass:$GPG_ENCPHRASE -in ci/pubring.gpg.enc -out ci/deploy/pubring.gpg -pbkdf2 -d
     openssl aes-256-cbc -pass pass:$GPG_ENCPHRASE -in ci/secring.gpg.enc -out ci/deploy/secring.gpg -pbkdf2 -d
-    gpg --fast-import ci/deploy/pubring.gpg
-    gpg --fast-import ci/deploy/secring.gpg
+    gpg --batch --fast-import ci/deploy/pubring.gpg
+    gpg --batch --fast-import ci/deploy/secring.gpg
 
     mvn deploy -P ossrh --settings ci/mvnsettings.xml
     # delete decrypted keys
