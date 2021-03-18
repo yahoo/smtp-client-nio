9dfecab1a2c2e65b156535818d6bb7e165861a27
Fan Su <fansu.dev@gmail.com>
2021-03-18 02:00:23 -0700

    Remove extra gpg settings

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 if [ "${TRAVIS_BRANCH}" == 'master' ] && [ "${TRAVIS_PULL_REQUEST}" == 'false' ]; then
-    export GPG_TTY=$(tty)
     mkdir ci/deploy
 
     openssl aes-256-cbc -pass pass:$GPG_ENCPHRASE -in ci/pubring.gpg.enc -out ci/deploy/pubring.gpg -pbkdf2 -d
