5a6b3018726d9dbcecc7be5506e7f7ec34bf1165
Fan Su <fansu.dev@gmail.com>
2021-03-17 22:30:45 -0700

    Update gpg encryption file

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 if [ "${TRAVIS_BRANCH}" == 'master' ] && [ "${TRAVIS_PULL_REQUEST}" == 'false' ]; then
+    export GPG_TTY=$(tty)
     mkdir ci/deploy
-    openssl aes-256-cbc -pass pass:$GPG_ENCPHRASE -in ci/pubring.gpg.enc -out ci/deploy/pubring.gpg -d
-    openssl aes-256-cbc -pass pass:$GPG_ENCPHRASE -in ci/secring.gpg.enc -out ci/deploy/secring.gpg -d
+    chmod 0400 ci/deploy
+
+    openssl aes-256-cbc -pass pass:$GPG_ENCPHRASE -in ci/pubring.gpg.enc -out ci/deploy/pubring.gpg -pbkdf2 -d
+    openssl aes-256-cbc -pass pass:$GPG_ENCPHRASE -in ci/secring.gpg.enc -out ci/deploy/secring.gpg -pbkdf2 -d
     gpg --fast-import ci/deploy/pubring.gpg
     gpg --fast-import ci/deploy/secring.gpg
 
