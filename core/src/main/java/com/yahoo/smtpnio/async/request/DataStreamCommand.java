ed10c4f399174abf1099aa297ee24c21c4eb1aee
Benoit Tellier <btellier@linagora.com>
2023-04-22 22:09:06 +0700

    Remove unused import

--- core/src/main/java/com/yahoo/smtpnio/async/request/DataStreamCommand.java
+++ core/src/main/java/com/yahoo/smtpnio/async/request/DataStreamCommand.java
@@ -9,7 +9,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.SequenceInputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.function.Supplier;
 
 import javax.annotation.Nonnull;
